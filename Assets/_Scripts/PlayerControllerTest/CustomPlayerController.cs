29814e334e83a2d74b25549d4bfa0fef0ac78162
DESKTOP-99CV4K8\qpalz <qpalzm60515@gmail.com>
2025-01-13 00:19:30 +0900

    카메라 위치 조정 완료. 현재 스냅 턴으로 높낮이를 조절중인데, readvalue 값이 입력되는 컨트롤러의 조이스틱 지점부터 앉기 일어서기가 가능해야, 정상적으로 작동하는 구조로 돼 있습니다. 기획서에서 원하는 방식으로 하려면 개별적으로 inputaction을 하나 만들어야할 듯 하네요. 추가로 apk 빌드퀄리티를 높여도 아웃라인 적용이 안되는 것을 확인했습니다. 일단 퀄리티 문제는 아니고, 쉐이더 문제일 가능성이 큽니다.

--- Assets/_Scripts/PlayerControllerTest/CustomPlayerController.cs
+++ Assets/_Scripts/PlayerControllerTest/CustomPlayerController.cs
@@ -10,6 +10,7 @@ using UnityEngine.Android;
 using UnityEngine.XR.Content.Interaction;
 using UnityEngine.XR.Interaction.Toolkit;
 using Unity.XR.CoreUtils;
+using TMPro;
 
 public class CustomPlayerController : MonoBehaviour
 {
@@ -18,44 +19,55 @@ public class CustomPlayerController : MonoBehaviour
     [SerializeField]
     private InputActionReference rightControllerAction;
     private Camera cam;
-
-    private bool isSit;
-
+    // private bool isSit;
     public Transform camOffset;
     public Transform camStartPos;
-    // public Transform mainCamPos;
+    public TextMeshProUGUI text;
 
-    [Tooltip("플레이어 중심부터 카메라가 나아갈 수 있는 최대 거리")]
-    public float maxDistance;
-    [Tooltip("Down Up Value == 최대 높이 - 최소 높이")]
-    public float downUpValue;
-    [Tooltip("카메라가 지정 범위를 벗어났을 시 컨트롤러 제어가능")]
-    public bool ctrlerControl;
-
+    private bool isPressed;
     private void Awake()
     {
         cam = GetComponentInChildren<Camera>();
-
     }
 
     private void Start()
     {
-        camOffset.position = camStartPos.position;
+        // camOffset.position = camStartPos.position;
     }
 
     private void OnEnable()
     {
-        rightControllerAction.action.performed += SitDown;
-        rightControllerAction.action.performed += StandUp;
-        isSit = false;
+        rightControllerAction.action.performed += DownUpAction;
+        rightControllerAction.action.started += DownUpAction;
+        rightControllerAction.action.canceled += DownUpAction;
+
+        // rightControllerAction.action.started += SitDown;
+        // rightControllerAction.action.canceled += SitDown;
+        // rightControllerAction.action.started += StandUp;
+        // rightControllerAction.action.canceled += StandUp;
+        // isSit = false;
         // activePrimaryX.action.performed += Primary;
     }
 
     private void OnDisable()
     {
-        rightControllerAction.action.performed -= SitDown;
-        rightControllerAction.action.performed -= StandUp;
+        rightControllerAction.action.performed += DownUpAction;
+        rightControllerAction.action.started += DownUpAction;
+        rightControllerAction.action.canceled += DownUpAction;
+
+        // rightControllerAction.action.started -= SitDown;
+        // rightControllerAction.action.canceled -= SitDown;
+        // rightControllerAction.action.started -= StandUp;
+        // rightControllerAction.action.canceled -= StandUp;
         // activePrimaryX.action.performed -= Primary;
+    }
+
+    private void Update()
+    {
+        if (isPressed)
+        {
+
+        }
     }
 
     #region 콜백
@@ -66,58 +78,35 @@ public class CustomPlayerController : MonoBehaviour
     }
     private void SitDown(InputAction.CallbackContext context)
     {
+        // if (isSit == true) return;
         float valuey = context.ReadValue<Vector2>().y;
-        Debug.Log($"Sitdown : {valuey}");
         if (cam.transform.localPosition.y < -2) return;
-        if (valuey <= -1 && isSit == false)
+        if (valuey <= -0.9)
         {
             camOffset.position =
             new Vector3(camOffset.position.x, camOffset.position.y - 2, camOffset.position.z);
-            isSit = true;
+            // isSit = true;
         }
     }
 
     private void StandUp(InputAction.CallbackContext context)
     {
+
+        // if (isSit == false) return;
         float valuey = context.ReadValue<Vector2>().y;
-        Debug.Log($"standup : {valuey}");
-        if (valuey >= 1 && isSit)
+        if (valuey >= 0.9)
         {
             camOffset.position =
             new Vector3(camOffset.position.x, camOffset.position.y + 2, camOffset.position.z);
-            isSit = false;
+            // isSit = false;
+
         }
     }
-    #endregion
 
-    #region 미사용
-    private void Update()
+    private void DownUpAction(InputAction.CallbackContext context)
     {
-        // CtrlerControl();
+        Vector2 value = context.ReadValue<Vector2>();
+        text.text = value.ToString();
     }
-
-    //충동체 중심으로 카메라가 가면 안되도록.
-    // private void CtrlerControl()
-    // {   //다른 스크립트에서 범위제한 해제 == ctrlerControl = true
-    //     if (ctrlerControl == false) return;
-
-    //     //충돌체 중심부터 카메라의 거리까지 계산
-    //     Vector3 ccTrans = char_Ctrler.transform.position + new Vector3(0, 3, 0);
-    //     Vector3 camTrans = mainCamPos.position;
-    //     float distance = Vector3.Distance(camTrans, ccTrans);
-
-    //     if (distance > maxDistance)
-    //     {
-    //         locoManager.gameObject.SetActive(false);
-    //         xRInModalManager.leftController.SetActive(false);
-    //         xRInModalManager.rightController.SetActive(false);
-    //     }
-    //     if (distance < maxDistance)
-    //     {
-    //         locoManager.gameObject.SetActive(true);
-    //         xRInModalManager.leftController.SetActive(true);
-    //         xRInModalManager.rightController.SetActive(true);
-    //     }
-    // }
     #endregion
 }
