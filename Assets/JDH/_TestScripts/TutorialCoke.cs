a32bf5874929ae2070bb935670901f99e6c9ae0d
Jang-daehoon <jub701@naver.com>
2025-01-15 17:48:23 +0900

    회전 및 이동에 관한 튜토리얼에 따라 UI 활성화 비활성화 설정 완료, 탄산수 아이템 쓰레기통에 버릴 시 잡을 수 없는 오브젝트로 변경하도록 추가 완료.

--- Assets/JDH/_TestScripts/TutorialCoke.cs
+++ Assets/JDH/_TestScripts/TutorialCoke.cs
@@ -1,9 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class TutorialCoke : MonoBehaviour
 {
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("TrashCan"))
+        {
+            this.gameObject.GetComponent<XRGrabInteractable>().enabled = false;
+            FindObjectOfType<StageManager>().throwCokecan = true;
+            Debug.Log("해당 오브젝트는 더이상 잡을 수 없습니다.");
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        this.gameObject.GetComponent<XRGrabInteractable>().enabled = true;
+        FindObjectOfType<StageManager>().throwCokecan = false;
+        Debug.Log("해당 오브젝트가 상호작용이 가능해졌습니다.");
+    }
     // Start is called before the first frame update
     void Start()
     {
